3cd5723a139bd221a158ff3489c3c2e2cb312682
nobi-onway <doangiabao.dev@gmail.com>
2024-06-03 10:04:50 +0700

    apply object pooler

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -1,15 +1,35 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    private const float TOP_BOUND = 30.0f;
-    private const float BOTTOM_BOUND = -10.0f;
+    private float topBound = 30;
+    private float lowerBound = -10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        if (transform.position.z < TOP_BOUND && transform.position.z > BOTTOM_BOUND) return;
+        if (transform.position.z > topBound)
+        {
+            // Instead of destroying the projectile when it leaves the screen
+            //Destroy(gameObject);
 
-        Destroy(gameObject);
+            // Just deactivate it
+            gameObject.SetActive(false);
+
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+
     }
 }
